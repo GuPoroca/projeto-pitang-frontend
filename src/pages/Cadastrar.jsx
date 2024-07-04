d6b6f0ad03d6569220dbfafcee9846be93a5e4da
Poroca <gustavoporoca@gmail.com>
2024-07-04 11:58:57 -0300

    Refactor to use chakra UI

---
+++ src/pages/Cadastrar.jsx
@@ -0,0 +1,56 @@
+'use client'
+
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+export default function SimpleCard() {
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'3xl'}>Cadastre o seu agendamento</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            Para tomar a vacina contra o Covid-19
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="nome">
+              <FormLabel>Nome</FormLabel>
+              <Input type="nome" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,5 @@
-import { useForm, FormProvider } from 'react-hook-form';
-import FormInput from '../components/FormInput';
-
-const MyFormPage = () => {
-  const methods = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
+export default function Home() {
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <FormInput name="firstName" label="First Name" />
-      </form>
-    </FormProvider>
-  );
-};
-
-export default MyFormPage;
+    <h1>Home</h1>
+  )
+}
