7c0bed6f59a3cae190685ef284a54c8e5f0f0c00
Poroca <gustavoporoca@gmail.com>
2024-07-07 10:38:30 -0300

    More Agendamento Tests

--- src/__tests__/TabelaAgendamento.test.js
+++ src/__tests__/TabelaAgendamento.test.js
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent, getByRole } from "@testing-library/react";
 import TabelaAgendamento from "../components/TabelaAgendamento/TabelaAgendamento";
 
-const agendamentos = [
+const agendamentosMock = [
   {
     id: 1,
     nome: "Marcos Castro",
@@ -40,28 +40,28 @@ const agendamentos = [
   },
 ];
 
-const renderComponent = () => {
+const renderComponent = (agendamentosMock) => {
   return render(
-    <TabelaAgendamento agendamentos={agendamentos} numEachPage={5} />
+    <TabelaAgendamento agendamentos={agendamentosMock} numEachPage={5} />
   );
 };
 
 describe("<TabelaAgendamento />", () => {
   describe("TabelaAgendamento", () => {
     it("should render", () => {
-      const component = renderComponent();
+      const component = renderComponent(agendamentosMock);
       expect(component).toBeTruthy();
       screen.logTestingPlaygroundURL();
     });
 
     it("should appear users", () => {
-      renderComponent();
+      renderComponent(agendamentosMock);
       expect(screen.getByText("Marcos Castro")).toBeInTheDocument();
       expect(screen.getByText("Gustavo Poroca")).toBeInTheDocument();
       expect(screen.getByText("Keven Leone")).toBeInTheDocument();
     });
     it("should go to next page", () => {
-      renderComponent();
+      renderComponent(agendamentosMock);
       expect(screen.getByText("Marcos Castro")).toBeInTheDocument();
       fireEvent.click(screen.getByRole("button", { name: /next page/i }));
       const nonExistant = screen.queryByText("Marcos Castro");
@@ -69,7 +69,7 @@ describe("<TabelaAgendamento />", () => {
       expect(screen.getByText("Gusmodas Poroca")).toBeInTheDocument();
     });
     it("should query the text", () => {
-      renderComponent();
+      renderComponent(agendamentosMock);
       expect(screen.getByText("Marcos Castro")).toBeInTheDocument();
       fireEvent.change(screen.getByRole("textbox"), {
         target: { value: "Poroca" },
@@ -78,6 +78,12 @@ describe("<TabelaAgendamento />", () => {
       expect(nonExistant).toBeNull();
       expect(screen.getByText("Gustavo Poroca")).toBeInTheDocument();
       expect(screen.getByText("Gusmodas Poroca")).toBeInTheDocument();
+    });
+    it("should not break if there is no agendamentos", () => {
+      renderComponent([{}]);
+      expect(screen.getByText("Data de Nascimento")).toBeInTheDocument();
+      const nonExistant = screen.queryByText("Marcos Castro");
+      expect(nonExistant).toBeNull();
     });
   });
 });
