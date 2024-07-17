bbe3e76f6403b105281eb247db04eb29c92ab762
Poroca <gustavoporoca@gmail.com>
2024-07-17 15:30:15 -0300

    agendamentomocks names

--- src/__tests__/TabelaAgendamento.test.js
+++ src/__tests__/TabelaAgendamento.test.js
@@ -7,36 +7,42 @@ const agendamentosMock = [
     name: "Marcos Castro",
     dataNascimento: new Date().toLocaleDateString(),
     dataAgendamento: new Date().toLocaleString(),
+    statusAgendamento: "futuro",
   },
   {
     id: 2,
     name: "Keven Leone",
     dataNascimento: new Date().toLocaleDateString(),
     dataAgendamento: new Date().toLocaleString(),
+    statusAgendamento: "futuro",
   },
   {
     id: 3,
     name: "Gustavo Poroca",
     dataNascimento: new Date().toLocaleDateString(),
     dataAgendamento: new Date().toLocaleString(),
+    statusAgendamento: "futuro",
   },
   {
     id: 4,
-    name: "Modas Castro",
+    name: "Mateus Castro",
     dataNascimento: new Date().toLocaleDateString(),
     dataAgendamento: new Date().toLocaleString(),
+    statusAgendamento: "futuro",
   },
   {
     id: 5,
-    name: "Keven Modas",
+    name: "Lucas Maciel",
     dataNascimento: new Date().toLocaleDateString(),
     dataAgendamento: new Date().toLocaleString(),
+    statusAgendamento: "futuro",
   },
   {
     id: 6,
-    name: "Gusmodas Poroca",
+    name: "Gabriel Poroca",
     dataNascimento: new Date().toLocaleDateString(),
     dataAgendamento: new Date().toLocaleString(),
+    statusAgendamento: "futuro",
   },
 ];
 
@@ -48,6 +54,7 @@ const renderComponent = (agendamentosMock) => {
 
 describe("<TabelaAgendamento />", () => {
   describe("TabelaAgendamento", () => {
+
     it("should render", () => {
       const component = renderComponent(agendamentosMock);
       expect(component).toBeTruthy();
@@ -60,30 +67,35 @@ describe("<TabelaAgendamento />", () => {
       expect(screen.getByText("Gustavo Poroca")).toBeInTheDocument();
       expect(screen.getByText("Keven Leone")).toBeInTheDocument();
     });
+
     it("should go to next page", () => {
       renderComponent(agendamentosMock);
       expect(screen.getByText("Marcos Castro")).toBeInTheDocument();
       fireEvent.click(screen.getByRole("button", { name: /next page/i }));
       const nonExistant = screen.queryByText("Marcos Castro");
       expect(nonExistant).toBeNull();
-      expect(screen.getByText("Gusmodas Poroca")).toBeInTheDocument();
+      expect(screen.getByText("Gabriel Poroca")).toBeInTheDocument();
     });
+
     it("should query the text", () => {
       renderComponent(agendamentosMock);
       expect(screen.getByText("Marcos Castro")).toBeInTheDocument();
       fireEvent.change(screen.getByRole("textbox"), {
         target: { value: "Poroca" },
       });
+
       const nonExistant = screen.queryByText("Marcos Castro");
       expect(nonExistant).toBeNull();
       expect(screen.getByText("Gustavo Poroca")).toBeInTheDocument();
-      expect(screen.getByText("Gusmodas Poroca")).toBeInTheDocument();
+      expect(screen.getByText("Gabriel Poroca")).toBeInTheDocument();
     });
+
     it("should not break if there is no agendamentos", () => {
       renderComponent();
       expect(screen.getByText("Data de Nascimento")).toBeInTheDocument();
       const nonExistant = screen.queryByText("Marcos Castro");
       expect(nonExistant).toBeNull();
     });
+
   });
 });
